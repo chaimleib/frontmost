432d139159ca6e203b714ad2c28e79eeda4dab02
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2014-03-10 22:20:01 -0700

    Initial Commit

---
+++ frontmost/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  frontmost
+//
+//  Created by Chaim-Leib Halbert on 3/10/14.
+//  Copyright (c) 2014 Vivalion Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
