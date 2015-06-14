fe596c6467901c2d0a66bbde9ee482565fb739be
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-06-14 13:32:58 -0700

    first working

---
+++ README.md
@@ -0,0 +1,6 @@
+# fmost
+
+This Mac CLI program tells you which GUI app is in front.
+
+Built on OS X 10.9.
+
--- frontmost/main.m
+++ frontmost/main.m
@@ -6,17 +6,26 @@
 //  Copyright (c) 2014 Vivalion Software. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+//#import <Foundation/Foundation.h>
+#import <Appkit/AppKit.h>
+#include <stdio.h>
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        NSWorkspace* workspace            = [NSWorkspace sharedWorkspace];
+        NSDictionary* currentAppInfo      = [workspace activeApplication];
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int pid = [[currentAppInfo objectForKey: @"NSApplicationProcessIdentifier"] intValue];
+        NSString* path = [currentAppInfo objectForKey:@"NSApplicationPath"];
+        NSString* name = [currentAppInfo objectForKey:@"NSApplicationName"];
+        NSString* appString = [NSString stringWithFormat:@"%i:%@:%@", pid, name, path, nil];
         
+        printf("%s\n", appString.UTF8String);
     }
     return 0;
 }
 
+// To watch for application switching:
+// [[workspace notificationCenter] addObserver:watcher selector:@selector(foremostAppActivated:) name:NSWorkspaceDidActivateApplicationNotification object:nil];
